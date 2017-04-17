87d2d372dcdd6125ee935f7aea351917b00588f8
Fabio Zanini <fabio.zanini@fastmail.fm>
2017-04-17 16:48:53 -0700

    better colors

--- pngrec.py
+++ pngrec.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
             if len(xr) == 1:
                 xr = [xr[0], xr[0] + 0.1]
                 yr = [yr[0], yr[0]]
-            hr = ax.plot(xr, yr[:len(xr)], color='darkred', lw=2)
+            hr = ax.plot(xr, yr[:len(xr)], color='#cc0033', lw=2)
 
         yg = ys[len(x):]
         xg = [xi for xi, ygi in zip(x, yg) if ygi is not None]
@@ -56,7 +56,7 @@ if __name__ == '__main__':
             if len(xg) == 1:
                 xg = [xg[0], xg[0] + 0.1]
                 yg = [yg[0], yg[0]]
-            hg = ax.plot(xg, yg[:len(xg)], color='green', lw=2)
+            hg = ax.plot(xg, yg[:len(xg)], color='#009900', lw=2)
 
         # Save and reopen duplicate
         from io import BytesIO
@@ -78,31 +78,44 @@ if __name__ == '__main__':
     def plot_distance_line(ds):
         fig, ax = plt.subplots()
         from matplotlib import cm
-        colors = cm.jet(np.linspace(0, 1, ds.shape[0]))
-        x = np.linspace(0, 35, ds.shape[1])
+        colors = cm.jet(np.linspace(0, 1, len(ds)))
         for d, color in zip(ds, colors):
-            if d.mask.any():
-                continue
-            ax.plot(x, d, lw=2, color=color)
+            x = d['ys']
+            y = d['distances']
+            ax.plot(x, y, lw=2, color=color)
 
         plt.ion()
         plt.show()
 
 
     # Minimize distance step by step
+    from operator import itemgetter
     prep = prepare_png()
     n_points = 100
     ys = [None for y in ys0]
-    y_scans = np.linspace(0, 35, n_points)
-    ds = np.ma.masked_all((len(ys), n_points))
+    ds = []
     for i in range(len(ys)):
         print('Minimizing i+1='+str(i+1)+' of '+str(len(ys)))
+        dsi = []
+        if i == 0:
+            y_scans = np.linspace(0, 35, 100)
+        else:
+            y_scans = np.linspace(0, 35, 100)
         for iy, y_scan in enumerate(y_scans):
             ys[i] = y_scan
-            ds[i, iy] = distance_pngs(ys, fig=prep['fig'], ax=prep['ax'])
-        ys[i] = y_scans[ds[i].argmin()]
-        if i == 31:
+            d = {'distance': distance_pngs(ys, fig=prep['fig'], ax=prep['ax']),
+                 'y': y_scan}
+            dsi.append(d)
+        dmin = min(dsi, key=itemgetter('distance'))
+        if (i > 0) and (dmin['distance'] > ds[-1]['distance_min'] * 1.1):
             break
+        ys[i] = dmin['y']
+        dtot = {'distance_min': dmin['distance'],
+                'y_min': dmin['y'],
+                'distances': [d['distance'] for d in dsi],
+                'ys': [d['y'] for d in dsi]}
+        ds.append(dtot)
+
     plot_distance_line(ds)
     yr = ys[:len(x)]
     yg = ys[len(x):]
@@ -113,7 +126,7 @@ if __name__ == '__main__':
     # Plot final figure
     fin = prepare_png()
     fin['ax'].plot(x, yr, lw=2, color='darkred')
-    #fin['ax'].plot(x, yg, lw=2, color='green')
+    fin['ax'].plot(x, yg, lw=2, color='green')
 
     plt.ion()
     plt.show()
